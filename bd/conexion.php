f4f125398352f383f5fc03e433d82b7c06195dde
Liz1908 <111602216+Liz1908@users.noreply.github.com>
2023-09-22 21:48:12 -0600

    Update conexion.php

--- bd/conexion.php
+++ bd/conexion.php
@@ -3,8 +3,8 @@ class Conexion{
     public static function Conectar() {        
         define('servidor', 'localhost');
         define('nombre_bd', 'login');
-        define('usuario', 'root');
-        define('password', '');					        
+        define('usuario', 'liz');
+        define('password', '12345');					        
         $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');			
         try{
             $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);			
